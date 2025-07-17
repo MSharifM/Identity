ad428e8e04337fc2aa8f5f0e0f6a597e75202c89
MSharifM <MSharifMdev@gmail.com>
2025-07-17 09:45:34 +0330

    create controller & view for manage users

---
+++ Identity/ViewModels/ManageUser/AddUserToRoleViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Identity.ViewModels.ManageUser
+{
+    public class AddUserToRoleViewModel
+    {
+    }
+}
---
+++ Identity/ViewModels/ManageUser/IndexViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Identity.ViewModels.ManageUser
+{
+    public class IndexViewModel
+    {
+        public string Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Email { get; set; }
+    }
+}
