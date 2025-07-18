3505e6b209853c8328038a06873de9a954ea470e
MSharifM <MSharifMdev@gmail.com>
2025-07-18 10:34:19 +0330

    create user & role manager

--- Identity/ViewModels/ManageUser/AddUserToRoleViewModel.cs
+++ Identity/ViewModels/ManageUser/AddUserToRoleViewModel.cs
@@ -2,5 +2,20 @@
 {
     public class AddUserToRoleViewModel
     {
+        public AddUserToRoleViewModel()
+        {
+            UserRoles = new List<UserRolesViewModel>();
+        }
+
+        public string UserId { get; set; }
+
+        public List<UserRolesViewModel> UserRoles { get; set; }
+    }
+
+    public class UserRolesViewModel
+    {
+        public string RoleName { get; set; }
+
+        public bool IsSelected { get; set; }
     }
 }
